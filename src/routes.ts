dc94dd908ebe1cfbba6788bbf0856e1719e1eb8e
Henrique Teles Ribeiro <henrique25.teles@hotmail.com>
2020-01-05 16:26:57 -0300

    Routes configuradas sem tsx

--- src/routes.ts
+++ src/routes.ts
@@ -1,20 +1,17 @@
-import React from 'react'
 import {createAppContainer, createSwitchNavigator} from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 
 import PageLogin from 'pages/login'
 import PageConfig from 'pages/configuracao'
-import PageIniciarApontamento, {navigationOptions as iniciarApontamentoNavigationOptions} from 'pages/apontamento/'
+import PageIniciarApontamento, {navigationOptions as navigationOptionsIniciarApontamento} from 'pages/apontamento/'
 import PageApontamentoItens from 'pages/apontamentoItens'
 import PageApontamentoQuantidades from 'pages/apontamentoQuantidades'
 import Colors from 'shared/styles/Colors'
-import { Icon, IconNode } from 'react-native-elements'
-import HeaderRight from 'shared/components/headerRightMenu'
 
 const MainPage = createStackNavigator({
     IniciarApontamento: {
         screen: PageIniciarApontamento,
-        navigationOptions: iniciarApontamentoNavigationOptions,
+        navigationOptions: navigationOptionsIniciarApontamento,
     },
     ApontamentoItens: {
         screen: PageApontamentoItens,
